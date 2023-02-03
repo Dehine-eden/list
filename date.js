38ca31a74867053318738180155199f9e537b234
Dehine-eden <119399020+Dehine-eden@users.noreply.github.com>
2023-02-03 14:14:05 +0300

    Add files via upload

---
+++ date.js
@@ -0,0 +1,32 @@
+//jshint esversion:6
+
+exports.getDate = function(){
+
+const today = new Date();
+
+const options = {
+    weekday: "long",
+    month: "long",
+    year:"numeric",
+    day:"numeric"
+    
+};
+
+const day = today.toLocaleDateString("en-US", options);
+
+return  today.toLocaleDateString("en-US", options);
+
+};
+
+exports.getDay = function(){
+
+const today = new Date();
+
+const options = {
+    weekday: "long",
+    
+};
+
+return  today.toLocaleDateString("en-US", options);
+
+};
